7709509b8cba93cc0fbe35c011685fcdde5a69d9
mohdalthafne <104372337+mohdalthafne@users.noreply.github.com>
2023-12-26 18:10:36 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,6 @@ let menuBar = document.querySelector('.navbar');
 let menuBtn = document.querySelector('.fa-ellipsis-vertical').onclick = () =>{
     menuBar.classList.toggle('show');
 }
-window.onscroll() = () =>{
-    menuBar.classList.remove('show');
+window.onscroll = () =>{
+   menuBar.classList.remove('show');
 }
