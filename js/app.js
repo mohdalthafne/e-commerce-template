fc70f08ccd9544f813ab51ec2872dbdf08c04903
mohdalthafne <104372337+mohdalthafne@users.noreply.github.com>
2023-12-26 18:05:42 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let menuBar = document.querySelector('.navbar');
+
+let menuBtn = document.querySelector('.fa-ellipsis-vertical').onclick = () =>{
+    menuBar.classList.toggle('show');
+}
+window.onscroll() = () =>{
+    menuBar.classList.remove('show');
+}
